45139348a814c1bdd1a9a0552354eeaed5220b2b
Lena <lenafabr@gmail.com>
2017-06-14 13:57:46 -0700

    minor changes

--- source/work20170607.m
+++ source/work20170607.m
@@ -3,12 +3,12 @@
 options = struct();
 options.nmito = 14*5;
 
-c0 = 0.01
+c0 = 0.4
 
 options.L = 500;
 options.D = 140;
-options.kg = 0.2;
-options.Km = 0.1/c0*100;
+options.kg = 0.2*10;
+options.Km = 0.1/c0;
 %options.startpos = 50;
 options.pstartwalk = 1;
 options.nstep = 1e5;
@@ -16,8 +16,8 @@ options.nstep = 1e5;
 options.showevery = 100;
 
 options.cend=1;
-options.ks = 100*(1/4.8*1e-6)*c0*(10^-3*6e23/1000/1e12*4^2);
-options.kw = 0.2;
+options.ks = (1/4.8*1e-6)*c0*(10^-3*6e23/1000/1e12*4^2);
+options.kw = 0.1;
 options.delt=5e-2;
 %
 %options.startgluc = gluc;
