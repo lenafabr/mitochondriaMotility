2d8721da979174c01173e0e8563e77cc5d32b69a
Anamika Agrawal <anamika@pc0.koslover>
2018-01-29 12:29:01 -0800

    work files etc

--- Anamika/work20180126.m
+++ Anamika/work20180126.m
@@ -36,7 +36,7 @@ save (filename);
 
 %% concatenate mitopos_dis to one long array
 mitodis = reshape(mitopos_dis,[1,size(mitopos_dis,1)*size(mitopos_dis,2)]);
-hist_mito = histogram(mitodis,30,'Normalization','pdf');
+hist_mito = histogram(mitodis,20,'Normalization','pdf');
 %hold on
 %histogram(mitodis,100,'Normalization','pdf');
 %hold off;
@@ -55,9 +55,9 @@ options.dodisplay=0;
 options.showevery=100;
 options.nmito=750;
 
-options.delt = 1e-5;
+options.delt = 1e-6;
 options.nstep = 1e7;
-options.gpts = 100;
+options.gpts = 500;
 [gluc_itr,Tmito_itr,Smito_itr,Smito_int_itr,normdtg,gluc_init,opt,xpos,lmdh,ftc] = runiterativesims(options);
 
 dx = xpos(2)-xpos(1);
